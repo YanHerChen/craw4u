8c12b5ea52e861e9ca495a24aa39acfdfe776aa0
routedan <dannypsnl@gmail.com>
2018-02-25 22:07:28 +0800

    clean: doc of `Result` & `Get`

--- http.go
+++ http.go
@@ -4,11 +4,15 @@ import (
 	"net/http"
 )
 
+// Result contains data user need
 type Result struct {
+	// StatusCode is 200, 404 these http code
 	StatusCode int
-	Err        error
+	// Err is the error that happened when trying connecting to url
+	Err error
 }
 
+// Get return response from url
 func Get(url string) *Result {
 	r, e := http.Get(url)
 	return &Result{
